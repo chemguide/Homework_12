e33a2f38b2cd51b69190fa3dcca4d5c81b36d0ab
chemguide <argentum198@gmail.com>
2024-05-07 11:17:37 +0500

    Вернул класс Main Облагородил вид кода

--- src/Author.java
+++ src/Author.java
@@ -1,13 +1,16 @@
 public class Author {
     private String authorName;
     private String authorSurname;
+
     public Author(String authorName, String authorSurname) {
         this.authorName = authorName;
         this.authorSurname = authorSurname;
     }
+
     public String getAuthorName() {
         return this.authorName;
     }
+
     public String getAuthorSurname() {
         return this.authorSurname;
     }
--- src/Book.java
+++ src/Book.java
@@ -2,11 +2,13 @@ public class Book {
     private String bookName;
     private Author author;
     private int publishYear;
+
     public Book(String bookName, Author author, int publishYear) {
         this.bookName = bookName;
         this.author = author;
         this.publishYear = publishYear;
     }
+
     public String getBookName() {
         return this.bookName;
     }
@@ -18,6 +20,7 @@ public class Book {
     public int getPublishYear() {
         return this.publishYear;
     }
+
     public void setPublishYear(int year) {
         this.publishYear = year;
     }
